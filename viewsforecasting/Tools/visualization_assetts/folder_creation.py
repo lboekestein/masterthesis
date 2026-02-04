4e04c6a822b449416e935c7b461a6cd03dec04ec
Luuk Boekestein <125049100+lboekestein@users.noreply.github.com>
2026-02-04 15:41:33 +0100

    add clone of fatalities002 model

---
+++ viewsforecasting/Tools/visualization_assetts/folder_creation.py
@@ -0,0 +1,10 @@
+from Tools.visualization_assetts.config2 import *
+
+Monthly_updates = Mydropbox + 'DataReleases/MonthlyUpdates/'
+label_version = f'{dev_id}_{model_wanted}_{vid2date_version2(EndOfHistory)}_{model_attempt}'
+master_folder = Monthly_updates + label_version
+
+#new folderpath
+def make_folders():
+    make_folders_complete_set(master_folder)
+    print(f"{user}, folders created")
